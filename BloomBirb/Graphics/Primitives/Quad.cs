27a3941657f9daa2b6876b5f26fe6ac9a7f7f8ba
Derrick Timmermans <derrick.timmermans@outlook.com>
2023-02-01 17:25:45 +0100

    Create Quad primitive

---
+++ BloomBirb/Graphics/Primitives/Quad.cs
@@ -0,0 +1,37 @@
+using System.Numerics;
+using System.Runtime.InteropServices;
+using BloomBirb.Extensions;
+
+namespace BloomBirb.Graphics.Primitives;
+
+[StructLayout(LayoutKind.Sequential)]
+public readonly struct Quad
+{
+    public readonly Vector2 TopLeft;
+    public readonly Vector2 BottomLeft;
+    public readonly Vector2 BottomRight;
+    public readonly Vector2 TopRight;
+
+    public Quad(Vector2 topLeft, Vector2 bottomLeft, Vector2 bottomRight, Vector2 topRight)
+    {
+        TopLeft = topLeft;
+        BottomLeft = bottomLeft;
+        BottomRight = bottomRight;
+        TopRight = topRight;
+    }
+
+    public Quad(float x, float y, float width, float height)
+    {
+        TopLeft = new Vector2(x, y);
+        BottomLeft = new Vector2(x, y + height);
+        BottomRight = new Vector2(x + width, y + height);
+        TopRight = new Vector2(x + width, y);
+    }
+
+    public static Quad operator *(Quad quad, Matrix3 transformation) => new(
+            quad.TopLeft.Transform(transformation),
+            quad.BottomLeft.Transform(transformation),
+            quad.BottomRight.Transform(transformation),
+            quad.TopRight.Transform(transformation)
+        );
+}
