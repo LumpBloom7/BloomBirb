18feb53943e3b64e6d1de81e6d785aa062e0ef40
Derrick Timmermans <derrick.timmermans@outlook.com>
2023-04-15 17:00:17 +0200

    Add BMFont binary decode

---
+++ BloomBirb/Font/BMFont/DescriptorBlocks/FontInfo.cs
@@ -0,0 +1,49 @@
+using System.Text;
+
+namespace BloomBirb.Font.BMFont.DescriptorBlocks;
+
+public readonly record struct FontInfo
+{
+    [Flags]
+    public enum FontFlags
+    {
+        None = 0,
+        Smooth = 1 << 7,
+        Unicode = 1 << 6,
+        Italic = 1 << 5,
+        Bold = 1 << 4,
+        FixedHeight = 1 << 3,
+    }
+
+    public short FontSize { get; private init; }
+    public FontFlags BitField { get; private init; }
+    public ushort Charset { get; private init; }
+    public ushort StretchH { get; private init; }
+    public ushort Antialiasing { get; private init; }
+    public byte PaddingUp { get; private init; }
+    public byte PaddingRight { get; private init; }
+    public byte PaddingDown { get; private init; }
+    public byte PaddingLeft { get; private init; }
+    public byte SpacingHoriz { get; private init; }
+    public byte SpacingVert { get; private init; }
+    public byte Outline { get; private init; }
+    public string FontName { get; private init; }
+
+    public FontInfo(ReadOnlySpan<byte> span)
+    {
+        FontSize = (short)(span[0] + (span[1] << 8));
+        BitField = (FontFlags)span[2];
+        Charset = span[3];
+        StretchH = (ushort)(span[4] + (span[5] << 8));
+        Antialiasing = span[6];
+        PaddingUp = span[7];
+        PaddingRight = span[8];
+        PaddingDown = span[9];
+        PaddingLeft = span[10];
+        SpacingHoriz = span[11];
+        SpacingVert = span[12];
+        Outline = span[13];
+        FontName = Encoding.Default.GetString(span.Slice(14, span.Length - 15)); // Remove the trailing null char
+    }
+
+}
---
+++ BloomBirb/Font/BMFont/Font.cs
@@ -0,0 +1,96 @@
+using System.Text;
+using BloomBirb.Font.BMFont.DescriptorBlocks;
+
+namespace BloomBirb.Font.BMFont;
+
+public class Font
+{
+    public readonly FontInfo FontInfo;
+    public readonly Common CommonInfo;
+    public readonly string[] PageNames;
+    public readonly Dictionary<uint, CharacterInfo> Characters = new Dictionary<uint, CharacterInfo>();
+    public readonly Dictionary<uint, Dictionary<uint, KerningPair>> KerningPairs = new();
+
+    public Font(Stream stream)
+    {
+        if (!isValidFontStream(stream))
+            throw new InvalidDataException("Font provided is not a valid BMF format.");
+
+        while (stream.CanRead)
+        {
+            int blockType = stream.ReadByte();
+
+            if (blockType == -1) // End of stream
+                break;
+
+            byte[] length = new byte[4];
+            stream.ReadExactly(length, 0,4);
+            int blockLength = length[0] + (length[1] << 8) + (length[2] << 16) + (length[3] << 24);
+            byte[] blockData = new byte[blockLength];
+            stream.ReadExactly(blockData, 0, blockLength);
+
+            switch (blockType)
+            {
+                case 1:
+                    FontInfo = new FontInfo(blockData);
+                    break;
+
+                case 2:
+                    CommonInfo = new Common(blockData);
+                    break;
+
+                case 3:
+                    PageNames = new string[CommonInfo.Pages];
+                    int stringLength = blockLength / PageNames.Length-1;
+                    for (int i = 0; i < PageNames.Length; ++i)
+                    {
+                        int start = PageNames.Length * i;
+                        PageNames[i] = Encoding.Default.GetString(blockData.AsSpan(start, stringLength));
+                    }
+
+                    break;
+                case 4:
+                {
+                    int n = blockLength / 20;
+                    for (int i = 0; i < n; ++i)
+                    {
+                        var charInfo = new CharacterInfo(blockData.AsSpan(i * 20, 20));
+                        Characters.Add(charInfo.Id, charInfo);
+                    }
+
+                    break;
+                }
+
+                case 5:
+                {
+                    int n = blockLength / 10;
+                    for (int i = 0; i < n; ++i)
+                    {
+                        var kerningPair = new KerningPair(blockData.AsSpan(i * 10, 10));
+
+                        if (!KerningPairs.TryGetValue(kerningPair.First, out var nested))
+                            KerningPairs.Add(kerningPair.First, nested = new Dictionary<uint, KerningPair>());
+
+                        nested.Add(kerningPair.Second, kerningPair);
+                    }
+
+                    break;
+                }
+            }
+        }
+    }
+
+    private static readonly byte[] valid_header_template = { 66, 77, 70, 3 };
+
+    private static bool isValidFontStream(Stream stream)
+    {
+        if (!stream.CanRead)
+            return false;
+
+        var header = new byte[4];
+
+        stream.ReadExactly(header, 0, 4);
+
+        return header.SequenceEqual(valid_header_template);
+    }
+}
