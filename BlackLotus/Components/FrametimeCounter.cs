e9c7a7939689e005bf0ec1244fc013f117bd6e24
Derrick Timmermans <derrick.timmermans@outlook.com>
2023-05-20 23:07:02 +0200

    Improve frametime display

--- BlackLotus/Components/FrametimeCounter.cs
+++ BlackLotus/Components/FrametimeCounter.cs
@@ -10,10 +10,20 @@ public class FrametimeCounter : SpriteText
     {
     }
 
+    private int frames = 0;
+    private double accumulator = 0;
+
     protected override void Update(double dt)
     {
         base.Update(dt);
 
-        Text = $"Frametime: {(dt * 1000):F2}ms";
+        ++frames;
+        accumulator += dt;
+
+        if (!(accumulator >= 0.5)) return;
+
+        Text = $"Frametime: {((accumulator / frames) * 1000):F2}ms";
+        accumulator = 0;
+        frames = 0;
     }
 }
