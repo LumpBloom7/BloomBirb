cb2d2182e14dbdb0955dcdf9bdf72251d781e254
Derrick Timmermans <derrick.timmermans@outlook.com>
2023-07-30 16:32:16 +0200

    Update test scene

--- BloomFramework/TestGame.cs
+++ BloomFramework/TestGame.cs
@@ -1,6 +1,7 @@
 using System.Numerics;
 using BloomFramework.Fonts.BMFont;
 using BloomFramework.Graphics;
+using BloomFramework.Graphics.Containers;
 using BloomFramework.Graphics.Text;
 using BloomFramework.ResourceStores;
 
@@ -17,6 +18,8 @@ public class TestGame : GameBase
     private DrawableSprite box = null!;
 
     private SpriteText frameTimeText = null!;
+
+    private Container container = null!;
 
     // Load everything needed for the scene to function
     // Also loads children afterwards without additional calls
@@ -40,6 +43,12 @@ public class TestGame : GameBase
         spriteShader.Bind();
         spriteShader.SetUniform("u_Texture0", 0);
 
+        container = new Container
+        {
+            RelativeSizeAxes = Axes.Both,
+            Size = Vector2.One,
+        };
+
         // Add our drawables
         AddRange(new Drawable[]
         {
@@ -50,6 +59,13 @@ public class TestGame : GameBase
                 Anchor = Anchor.MiddleCentre,
                 Origin = Anchor.MiddleCentre
             },
+            container = new Container
+            {
+                RelativeSizeAxes = Axes.Both,
+                Size = Vector2.One,
+                Origin = Anchor.MiddleCentre,
+                Anchor = Anchor.MiddleCentre
+            },
             frameTimeText = new SpriteText(spriteShader, font)
             {
                 Scale = new Vector2(0.33f),
@@ -57,6 +73,21 @@ public class TestGame : GameBase
                 Origin = Anchor.BottomLeft
             }
         });
+
+        for (int i = 0; i < 100000; ++i)
+        {
+            container.Add(new DrawableSprite(spriteShader)
+            {
+                Texture = textures.Get(getTextureStringFor(Random.Shared.Next(0, 5))),
+                Size = new Vector2(Random.Shared.NextSingle() * 300),
+                RelativePositionAxes = Axes.Both,
+                Anchor = Anchor.MiddleCentre,
+                Origin = Anchor.MiddleCentre,
+                Position = new Vector2(Random.Shared.NextSingle() - 0.5f, Random.Shared.NextSingle() - 0.5f),
+                Rotation = Random.Shared.NextSingle() * 360,
+                Colour = HsvToRgb(Random.Shared.NextSingle(), 1, 1)
+            });
+        }
     }
 
     private float hue = 0;
@@ -70,6 +101,7 @@ public class TestGame : GameBase
 
         frameTimeText.Text = $"Frametime: {dt * 1000:F2}ms";
         box.Rotation += (float)(dt * 90);
+        container.Rotation = box.Rotation;
         hue += (float)(dt * 0.125f);
 
         box.Colour = HsvToRgb(hue % 1, 1, 1);
@@ -78,20 +110,22 @@ public class TestGame : GameBase
         if (!(timeTrack > 2)) return;
         timeTrack %= 2;
 
-        // Add a random box to the scene every 2 secs
-        // Note that these sprites aren't "loaded", since they are just created
-        // These will be loaded on the first use, by the framework
-        Add(new DrawableSprite(shaders.Get("Texture", "Texture"))
-        {
-            Texture = textures.Get("a load of this"),
-            Size = new Vector2(Random.Shared.NextSingle() * 300),
-            RelativePositionAxes = Axes.Both,
-            Anchor = Anchor.MiddleCentre,
-            Origin = Anchor.MiddleCentre,
-            Position = new Vector2(Random.Shared.NextSingle() - 0.5f, Random.Shared.NextSingle() - 0.5f),
-            Rotation = Random.Shared.NextSingle() * 360,
-            Colour = HsvToRgb(Random.Shared.NextSingle(), 1, 1)
-        });
+
+
+        /*         // Add a random box to the scene every 2 secs
+                // Note that these sprites aren't "loaded", since they are just created
+                // These will be loaded on the first use, by the framework
+                Add(new DrawableSprite(shaders.Get("Texture", "Texture"))
+                {
+                    Texture = textures.Get("a load of this"),
+                    Size = new Vector2(Random.Shared.NextSingle() * 300),
+                    RelativePositionAxes = Axes.Both,
+                    Anchor = Anchor.MiddleCentre,
+                    Origin = Anchor.MiddleCentre,
+                    Position = new Vector2(Random.Shared.NextSingle() - 0.5f, Random.Shared.NextSingle() - 0.5f),
+                    Rotation = Random.Shared.NextSingle() * 360,
+                    Colour = HsvToRgb(Random.Shared.NextSingle(), 1, 1)
+                }); */
     }
 
     private static Vector4 HsvToRgb(float h, float s, float v)
@@ -106,4 +140,13 @@ public class TestGame : GameBase
 
         return new Vector4(f(5), f(3), f(1), 1);
     }
+
+    private static string getTextureStringFor(int number) => number switch
+    {
+        1 => "funnyface",
+        2 => "kitty",
+        3 => "mike",
+        4 => "sticky",
+        _ => "empty"
+    };
 }
